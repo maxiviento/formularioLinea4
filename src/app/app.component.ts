588a9685e5839773a52655a17fc7048d108e5e45
edgarlunaa <edgaribarluna@gmail.com>
2021-02-12 14:42:45 -0300

    cambio crearPdf()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1458,75 +1458,111 @@ export class AppComponent {
 
   
 
-	createPdf() {
+  createPdf() {
 
-  if (this.form.valid) {
-    let modelo = Object.entries(this.model);
-    //
-    var doc = new jsPDF('p','mm','a4');
-    //
-    var img = new Image();
-    img.src = 'assets/cabecera.jpg';
-    doc.addImage(img, 'jpg', 0, 0);
-    //
-    doc.setFont('helvetica')
+    if (this.form.invalid) {
+      let modelo = Object.entries(this.model);
+      //
+      var doc = new jsPDF('p', 'mm', 'a4');
+      var img = new Image();
+      img.src = 'assets/cabecera.jpg';
+      doc.addImage(img, 'jpg', 0, 0);
 
-    let m = 30;
-    let y = 10;
-    let x = 15;
-    let i = 0; //
-    //var arr:JSON[];
+      doc.setFont('helvetica')
 
-    for (let seccion of modelo) {
+      let m = 30;
+      let y = 5;
+      let x = 15;
+      let i = 0; //
+      let ll = 90;
+      //var arr:JSON[];
 
-      let arr: any = seccion[1];
+      for (let seccion of modelo) {
 
-      y = y + 6;
-      doc.setFontSize(16);
-      doc.setTextColor(45);
-      doc.text(seccion[0], x, m + y); //nombre seccion
-      doc.line(x, m + y + 1, x + 180, m + y + 1);
-
-
-
-      for(var j = 0; j < arr.length;j++){
-        
-        //console.log(reg);
-        var res = [];
-        var z = 0;
-        for(var clave in arr[j]){
-          if (y > 240 && x === 110) {
-            doc.addPage();
-            doc.addImage(img, 'jpg', 0, 0);
-            m = 30;
-            y = 10;
-            x = 15;
-          }
-          i++;
-          if (i % 2 != 0){x = 15; y = y + 12;}
-          else {x = 110;}
-          doc.setFontSize(10);
-          doc.setDrawColor(100);
-
-          res.push([clave, arr[j][clave]]);
-          var registro:String[] = [clave, 'algo quee no se paso a string'];
-          try {
-            registro = res[z]; //paso los valores a string
-          } catch (e) {
-            console.log(e)
-          }
-          z++;
-          doc.text(registro[1], x, m+y); //valor
-          doc.line(x, m + y + 1, x + 90, m + y + 1); // linea horizontal
-          doc.setFontSize(8);
-          doc.setDrawColor(60);
-          doc.text(clave, x, m + y + 5); //key
+        let arr: any = seccion[1];
+        if (y > 240 ) {
+          doc.addPage();
+          doc.addImage(img, 'jpg', 0, 0);
+          m = 30;
+          y = 5;
+          x = 15;
         }
+
+        y = y + 6;
+        doc.setFontSize(16);
+        doc.setTextColor(45);
+        doc.text(seccion[0], x, m + y); //nombre seccion
+        doc.line(x, m + y + 1, x + 180, m + y + 1);
+
+        for (var j = 0; j < arr.length; j++) {
+
+          //console.log(reg);
+          var res = [];
+          var z = 0;
+          for (var clave in arr[j]) {
+            i++;
+            res.push([clave, arr[j][clave]]);
+            var registro: String[] = [clave, 'algo quee no se paso a string'];
+            try {
+              registro = res[z]; //paso los valores a string
+            } catch (e) {
+              console.log(e)
+            }
+            z++;
+            var texto = ''
+            //RESUELVO SI EL TEXTO ES LARGO O CORTO O SI ES DE UNA COLUMNA U OTRA
+            //console.log(registro[1])
+            texto = registro[1].toString()
+
+            var text_arr_aux = new Array
+            text_arr_aux = []
+            text_arr_aux = texto.split("",texto.length)
+            console.log(texto)
+            console.log(text_arr_aux)
+            var text_arr = new Array
+            text_arr = []
+            var texto_aux = ""
+            for(var jj = 0; jj < text_arr_aux.length; jj++){
+              texto_aux = texto_aux + text_arr_aux[jj]
+              if(jj%115==0 && jj != 0){
+                text_arr.push(texto_aux)
+                texto_aux = ""
+              }
+            }
+            text_arr.push(texto_aux)
+            console.log(texto_aux)
+            
+            
+            if (texto.length > 40) {x = 15; y = y + 12; i++; ll=180}
+            else { if (i % 2 != 0 || ll==180 ) { x = 15; y = y + 12; ll=90 }
+                  else { x = 110; ll=90 } }
+            //ACA PREGUNTO SI ESTOY SALIENDOME DE LA HOJA
+            if (y > 240) {
+              doc.addPage();
+              doc.addImage(img, 'jpg', 0, 0);
+              m = 30;
+              y = 5;
+              x = 15;
+            }
+            
+            doc.setFontSize(10);
+            doc.setDrawColor(100);
+            for (var ia = 0; ia < text_arr.length; ia++) {                
+              doc.text(text_arr[ia], x, m + y); //valor
+              y = y + 5             
+            }
+            y = y - 5
+            
+            doc.line(x, m + y + 1, x + ll, m + y + 1); // linea horizontal
+            doc.setFontSize(8);
+            doc.setDrawColor(60);
+            doc.text(clave, x, m + y + 5); //key
+          }
+        }
+        i = 0;
+        x = 15;
+        y = y + 12;
       }
-      i = 0;
-      x = 15;
-      y = y + 12;
-    }
     let nombreArchivo = '00000000000';
     nombreArchivo = this.model['Solicitante'][0]['CUIL'];
       doc.output('dataurlnewwindow');
